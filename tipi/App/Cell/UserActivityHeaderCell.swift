50a65a2b5a5ab50c572ab9cc251609db04468c74
jamal zare <jamal@jamals-macbook-air.local>
2018-07-11 16:49:17 +0430

    change file userAtivityHeaderCell.swift class and content to UserActivityHeaderCell.swift

--- tipi/App/Cell/UserActivityHeaderCell.swift
+++ tipi/App/Cell/UserActivityHeaderCell.swift
@@ -58,3 +58,41 @@ class UserActivityHeaderCell: BaseCell{
     
 }
 
+
+
+
+
+class UserActivityHeaderLayout: UICollectionViewFlowLayout{
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layoutAttributes = super.layoutAttributesForElements(in: rect)
+        let offsetY = collectionView!.contentOffset.y
+        let delta = abs(offsetY)
+        
+        for attr in layoutAttributes!{
+            if let elementKind = attr.representedElementKind{
+                
+                if elementKind == UICollectionElementKindSectionHeader{
+                    
+                    var frame = attr.frame
+                    if offsetY<0{
+                        frame.size.height = max(0, frame.size.height + delta)
+                        frame.origin.y = frame.minY - delta
+                    }else{
+                        frame.origin.y = frame.minY + (delta/2)
+                        attr.zIndex = -1
+                    }
+                    attr.frame = frame
+                    
+                }
+            }
+        }
+        return layoutAttributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+}
+
--- tipi/LIB/DetailCollectionViewLayout.swift
+++ tipi/LIB/DetailCollectionViewLayout.swift
@@ -1,44 +1,53 @@
-//
-//  DetailCollectionViewLayout.swift
-//  tipi
-//
-//  Created by Jamal on 7/11/18.
-//  Copyright © 2018 Jamal. All rights reserved.
-//
-
-import UIKit
 
 
-class UserActivityHeaderLayout: UICollectionViewFlowLayout{
-    
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let layoutAttributes = super.layoutAttributesForElements(in: rect)
-        let offsetY = collectionView!.contentOffset.y
-        let delta = abs(offsetY)
-        
-        for attr in layoutAttributes!{
-            if let elementKind = attr.representedElementKind{
-                
-                if elementKind == UICollectionElementKindSectionHeader{
-                    
-                    var frame = attr.frame
-                    if offsetY<0{
-                        frame.size.height = max(0, frame.size.height + delta)
-                        frame.origin.y = frame.minY - delta
-                    }else{
-                        frame.origin.y = frame.minY + (delta/2)
-                        attr.zIndex = -1
-                    }
-                    attr.frame = frame
-                    
-                }
-            }
-        }
-        return layoutAttributes
-    }
-    
-    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
-        return true
-    }
-    
-}
+//////////////////////////////////////////
+//                                      //
+//          this class add to           //
+//   UserActivityHeaderCell.swift file  //                      
+//                                      //
+//////////////////////////////////////////
+////
+////  DetailCollectionViewLayout.swift
+////  tipi
+////
+////  Created by Jamal on 7/11/18.
+////  Copyright © 2018 Jamal. All rights reserved.
+////
+//
+//import UIKit
+//
+//
+//class UserActivityHeaderLayout: UICollectionViewFlowLayout{
+//    
+//    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+//        let layoutAttributes = super.layoutAttributesForElements(in: rect)
+//        let offsetY = collectionView!.contentOffset.y
+//        let delta = abs(offsetY)
+//        
+//        for attr in layoutAttributes!{
+//            if let elementKind = attr.representedElementKind{
+//                
+//                if elementKind == UICollectionElementKindSectionHeader{
+//                    
+//                    var frame = attr.frame
+//                    if offsetY<0{
+//                        frame.size.height = max(0, frame.size.height + delta)
+//                        frame.origin.y = frame.minY - delta
+//                    }else{
+//                        frame.origin.y = frame.minY + (delta/2)
+//                        attr.zIndex = -1
+//                    }
+//                    attr.frame = frame
+//                    
+//                }
+//            }
+//        }
+//        return layoutAttributes
+//    }
+//    
+//    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+//        return true
+//    }
+//    
+//}
+
